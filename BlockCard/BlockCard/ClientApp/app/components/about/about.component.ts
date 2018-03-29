8af154e2ae17797a08bd82931b7252b073a5163f
Alex Hansen <alexhansen@outlook.com>
2018-03-28 17:13:21 -0700

    Swap about and home components
    Related Work Items: #40

--- BlockCard/BlockCard/ClientApp/app/components/about/about.component.ts
+++ BlockCard/BlockCard/ClientApp/app/components/about/about.component.ts
@@ -1,23 +1,8 @@
-import { Component, Inject } from '@angular/core';
-import { Http } from '@angular/http';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'about',
     templateUrl: './about.component.html'
 })
 export class AboutComponent {
-    public forecasts: WeatherForecast[];
-
-    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-        http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-            this.forecasts = result.json() as WeatherForecast[];
-        }, error => console.error(error));
-    }
-}
-
-interface WeatherForecast {
-    dateFormatted: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
 }
